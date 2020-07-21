7c9ff97c5d123f1ef5eb0ffba40e0e5723caf321
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-21 17:42:51 +0100

    adding list of moves to the page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Input from './input.js';
 
 var button = document.getElementById("enter");
 var userInput = document.getElementById("userinput")
+var list = document.querySelector("ul")
 
 var input = new Input()
 
@@ -10,8 +11,10 @@ var input = new Input()
 button.addEventListener("click", function(){
 
   if (userInput.value === "player.moveRight()") {
-    console.log("player.moveRight()");
     input.addInput("player.moveRight()")
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(userInput.value))
+    list.appendChild(li)
     console.log(input);
   } else {
     alert("Wrong move, try again")
