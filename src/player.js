574d10504c93515e63083b874b438a0af16755b4
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-21 15:31:50 +0100

    added moveRight() function that moves the block 30px right

--- src/player.js
+++ src/player.js
@@ -1,20 +1,32 @@
 export default class Player {
   constructor(gameHeight, gameWidth) {
     // game area size
-    this.gameHeight = gameHeight
-    this.gameWidth = gameWidth
+    this.gameHeight = gameHeight;
+    this.gameWidth = gameWidth;
     // object size
     this.height = 30;
     this.width = 30;
     // object position
     this.position = {
       x: 0,
-      y: this.gameHeight - this.height
-    }
+      y: this.gameHeight - this.height,
+    };
+    //movement value
+    this.movement = {
+      x: 30,
+      y: 30,
+    };
   }
 
   draw(ctx) {
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
+  update(deltaTime) {
+    if (!deltaTime) return;
+  }
+
+  moveRight() {
+    this.position.x += this.movement.x;
+  }
 }
