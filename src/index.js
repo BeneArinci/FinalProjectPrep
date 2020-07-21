0b5c5bf8529c0855f461b33993114c36dbcb37e4
Emanuele Pace <emanuele.10@outlook.it>
2020-07-21 17:26:31 +0100

    Track the player Input

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import Input from './input.js';
 
 
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput")
+var userInput = document.getElementById("userinput")
+
+var input = new Input()
+
 
 button.addEventListener("click", function(){
-  if (input.value === "player.moveRight()") {
+
+  if (userInput.value === "player.moveRight()") {
     console.log("player.moveRight()");
+    input.addInput("player.moveRight()")
+    console.log(input);
   } else {
     alert("Wrong move, try again")
   }
